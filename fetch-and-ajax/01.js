4e6a1c2cada7946b5251651aef4a005051e6161a
@gab0071 <jeicarm7@gmail.com>
2022-09-07 14:18:32 -0500

    fetch and promises

--- fetch-and-ajax/01.js
+++ fetch-and-ajax/01.js
@@ -8,21 +8,54 @@
 
 window.addEventListener('load', () => {
     let div_users = document.querySelector('#personas')
+    let div_janet = document.querySelector('#janet')
     let usuarios = []
     // esto es una promesa con lo cual utilizamos el metodo them() para recoger datos
-    fetch('https://reqres.in/api/users')
-        // esto es para convertir un obj que nos llegan en json directamente
+
+    // esto es para convertir un obj que nos llegan en json directamente
+    getUsers()
         .then((data) => data.json()) // captura los datos y convertirlos a json
         .then((users) => {
             usuarios = users.data
             console.log(usuarios)
+            usersList(users.data)
 
-            usuarios.map((user, i) => {
-                let nombre = document.createElement('h2')
-                nombre.innerHTML = `${i}. ${user.first_name} ${user.last_name} `
-                div_users.appendChild(nombre)
-
-                document.querySelector('.loading').style.display = 'none'
-            })
+            return getJanet()
         })
+        .then((data) => data.json())
+        .then((janet) => {
+            userJanet(janet.data)
+        })
+
+    function getUsers() {
+        return fetch('https://reqres.in/api/users')
+    }
+    function getJanet() {
+        return fetch('https://reqres.in/api/users/2')
+    }
+
+    function usersList(usuarios) {
+        usuarios.map((user, i) => {
+            let nombre = document.createElement('h2')
+            nombre.innerHTML = `${i}. ${user.first_name} ${user.last_name} `
+            div_users.appendChild(nombre)
+
+            document.querySelector('.loading').style.display = 'none'
+        })
+    }
+
+    function userJanet(user) {
+        let nombre = document.createElement('h2')
+        let avatar = document.createElement('img')
+
+        nombre.innerHTML = `${user.first_name} ${user.last_name} `
+        avatar.src = user.avatar
+        avatar.width = '160'
+        console.log(avatar)
+
+        div_janet.appendChild(nombre)
+        div_janet.appendChild(avatar)
+
+        document.querySelector('#janet .loading').style.display = 'none'
+    }
 })
